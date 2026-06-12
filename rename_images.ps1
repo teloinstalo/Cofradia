2be3eaa3ab10fb1be4aaf9b2de9a4ddbf8705f32
latanadelzorro <latanadelzorro@gmail.com>
2026-06-12 18:18:14 +0200

    Renombrar y organizar 159 imágenes provisionales; reemplazar placeholders picsum por fotos reales de la cofradía

---
+++ rename_images.ps1
@@ -0,0 +1,268 @@
+# Script de renombre de imágenes provisionales de la Cofradía
+# Mapeo de nombres originales a nombres descriptivos
+
+$renames = @{
+    # === ESCUDO ===
+    "062_ESCUDOMONTORO.jpg" = "escudo-montoro.jpg"
+    
+    # === FOTOS DEL CRISTO (lo más importante) ===
+    "089_11140069_559560680853115_607892587757869934_n.jpg" = "foto-cristo-salida-ermita-2014.jpg"
+    "091_Copia_de_IMG-20140419-WA0008.jpg" = "foto-cristo-procesion-2014.jpg"
+    "098_1982320_406763569485956_3147011949942888244_n.jpg" = "foto-cristo-ermita-2014.jpg"
+    "099_10920900_406763466152633_6858039349901576133_n.jpg" = "foto-cristo-ermita-altar-2014.jpg"
+    "107_10404382_762768507092743_359619930963499376_n.jpg" = "foto-cristo-ermita-virgen-2014.jpg"
+    "125_BkJk6CyIYAAeDl3.jpg" = "foto-ermita-exterior.jpg"
+    "126_1975210_282965495199098_1884179277_n.jpg" = "foto-cristo-angel-2014.jpg"
+    "127_1619375_282947905200857_486002677_n.jpg" = "foto-junta-gobierno-2014.jpg"
+    "128_BjaxSLHIAAAmHoW.jpg_large.jpg" = "foto-cristo-angel-2013.jpg"
+    "129_1459117_282948175200830_1994028121_n.jpg" = "foto-imposicion-medallas-2014.jpg"
+    "136_1425562_281792881970219_801695193_n.jpg" = "foto-semana-santa-montoro-2014.jpg"
+    "139_BeLeOQGIQAAUxVw.jpg" = "foto-reparto-juguetes-2014.jpg"
+    
+    # === FOTOS SOLIDARIAS (Dona un juguete) ===
+    "144_544193_10151784629773683_1436979437_n.jpg" = "foto-juguetes-hermanas-2013.jpg"
+    "145_BaZqE0jIAAAH-Ap.jpg" = "foto-juguetes-mesa-2013.jpg"
+    "146_Ba9SiMeIUAAlxGK.jpg_large.jpg" = "foto-juguetes-hermanas-2-2013.jpg"
+    "147_BbhVEx_IIAA5heJ.jpg" = "foto-juguetes-exposicion-2013.jpg"
+    "148_BcpR7zMIMAA9fT8.jpg" = "foto-juguetes-hermanas-3-2013.jpg"
+    "149_BcFY7wtCAAA4mjU.jpg" = "foto-juguetes-ninos-2013.jpg"
+    "150_BdFqYCyIYAAygEI.jpg" = "foto-juguetes-reparte-ilusion-2013.jpg"
+    "151_1486679_10151867028388683_10442246_n.jpg" = "foto-juguetes-hermanas-4-2013.jpg"
+    
+    # === CARTELES TRIDUOS ===
+    "010_Cartel_Triduo_2019.jpg" = "cartel-triduo-2019.jpg"
+    "020_Cartel_Triduo_2018.jpg" = "cartel-triduo-2018.jpg"
+    "029_cartel_2017.jpg" = "cartel-triduo-2017.jpg"
+    "055_cartel_triduo_2016.jpg" = "cartel-triduo-2016.jpg"
+    "085_cartel__triduo_2015.jpg" = "cartel-triduo-2015.jpg"
+    "121_triduo_2014.jpg" = "cartel-triduo-2014.jpg"
+    
+    # === CARTELES CATA DE VINO ===
+    "003_CARTEL_XXI_CATA_DE_VINO_2019.jpeg" = "cartel-cata-vino-2019.jpg"
+    "028_cartel_V_CATA_DE_VINO.jpg" = "cartel-cata-vino-2017.jpg"
+    "048_cartel_cata_de_vino_2016.jpg" = "cartel-cata-vino-2016.jpg"
+    "082_Cartel_Cata_de_Vino_2015.jpg" = "cartel-cata-vino-2015.jpg"
+    "113_cata_vino_resucitado2014.jpg" = "cartel-cata-vino-2014.jpg"
+    "156_cata_vino_resucitado_2013.jpg" = "cartel-cata-vino-2013.jpg"
+    
+    # === CARTELES DÍA DE LA BICICLETA ===
+    "004_CARTEL_VI_DIA_DE_LA_BICICLETA_2019.jpeg" = "cartel-dia-bicicleta-2019.jpg"
+    "027_CARTEL_V_DIA_DE_LA_BICICLETA_2017.jpg" = "cartel-dia-bicicleta-2017.jpg"
+    "046_CARTEL_DIA_DE_LA_BICICLETA_2016.jpg" = "cartel-dia-bicicleta-2016.jpg"
+    "080_CARTEL_DIA_DE_LA_BICI_2015.jpg" = "cartel-dia-bicicleta-2015.jpg"
+    "114_cartel_II_dia_bicicleta.jpg" = "cartel-dia-bicicleta-2014.jpg"
+    "159_CARTEL_DIA_BICI.JPG" = "cartel-dia-bicicleta-2013.jpg"
+    
+    # === CARTELES MARATÓN FÚTBOL ===
+    "160_CARTEL_FUTBOL_7_MARATON.jpg" = "cartel-maraton-futbol-2013.jpg"
+    "117_CARTEL_maraton_2014.jpg" = "cartel-maraton-futbol-2014.jpg"
+    "083_cartel_maraton_2015.jpg" = "cartel-maraton-futbol-2015.jpg"
+    
+    # === CARTELES VÍA LUCIS ===
+    "007_CARTEL_VIA_LUCIS_2019.jpeg" = "cartel-via-lucis-2019.jpg"
+    "019_Cartel_Via_Lucis_2018.jpeg" = "cartel-via-lucis-2018.jpg"
+    
+    # === CARTELES CRUZ DE MAYO ===
+    "057_CARTEL_CRUZ_DE_MAYO_2016.jpg" = "cartel-cruz-mayo-2016.jpg"
+    "058_CARTEL_ACTOS_CRUZ_DE_MAYO_2016.jpg" = "cartel-cruz-mayo-actos-2016.jpg"
+    "087_CARTEL_CRUZ_DE_MAYO_2015.jpg" = "cartel-cruz-mayo-2015.jpg"
+    "124_CARTEL_CRUZ_DE_MAYO_2014.JPG" = "cartel-cruz-mayo-2014.jpg"
+    
+    # === CARTELES CASETA FERIA ===
+    "079_CARTEL_CASETA_FERIA_2015.png" = "cartel-caseta-feria-2015.png"
+    "111_CARTEL_CASETA_FERIA.JPG" = "cartel-caseta-feria-2014.jpg"
+    "154_cartel_caseta_2013.jpg" = "cartel-caseta-feria-2013.jpg"
+    
+    # === CARTELES NAVIDAD / CHRISTMAS ===
+    "038_christmas_2016.jpg" = "cartel-navidad-2016.jpg"
+    "070_christmas_2015.jpg" = "cartel-navidad-2015.jpg"
+    "104_christmas_2014.JPG" = "cartel-navidad-2014.jpg"
+    "141_christmas_2013.jpg" = "cartel-navidad-2013.jpg"
+    
+    # === CARTELES IMPOSICIÓN MEDALLAS ===
+    "009_Imposicion_medallas_2019.jpg" = "cartel-imposicion-medallas-2019.jpg"
+    "018_imposicion_medallas.jpg" = "cartel-imposicion-medallas-2018.jpg"
+    "030_imposicion_de_medallas.jpg" = "cartel-imposicion-medallas-2017.jpg"
+    "056_IMPOSICI_25C3_2593N_DE_MEDALLAS_TRIDUO_2016.JPG" = "cartel-imposicion-medallas-2016.jpg"
+    "084_IMPOSICI_C3_93N_MEDALLAS_TRIDUO_2015.JPG" = "cartel-imposicion-medallas-2015.jpg"
+    "120_IMPOSICION_MEDALLAS_TRIDUO.JPG" = "cartel-imposicion-medallas-2014.jpg"
+    "133_CARTEL_IMPOSICION_MEDALLAS.JPG" = "cartel-imposicion-medallas-2013.jpg"
+    
+    # === CARTELES REUNIÓN NAZARENOS ===
+    "022_CARTEL_REUNION_NAZARENOS_2018.jpg" = "cartel-reunion-nazarenos-2018.jpg"
+    "032_CARTEL_REUNION_NAZARENOS_2017.jpg" = "cartel-reunion-nazarenos-2017.jpg"
+    "066_CARTEL_REUNION_NAZARENOS_2016.JPG" = "cartel-reunion-nazarenos-2016.jpg"
+    "097_CARTEL_REUNION_NAZARENOS_2015.JPG" = "cartel-reunion-nazarenos-2015.jpg"
+    "131_CARTEL_REUNION_NAZARENOS_2014.JPG" = "cartel-reunion-nazarenos-2014.jpg"
+    
+    # === CARTELES LOTERÍA ===
+    "026_LOTERIA_DE_NAVIDAD_2017.jpg" = "cartel-loteria-navidad-2017.jpg"
+    "043_loteria_navidad_2016.jpg" = "cartel-loteria-navidad-2016.jpg"
+    "078_LOTERIA_NAVIDAD_RESUCITADO_2015.JPG" = "cartel-loteria-navidad-2015.jpg"
+    "110_decimo_2014.JPG" = "cartel-loteria-navidad-2014.jpg"
+    "153_decimo2013.jpg" = "cartel-loteria-navidad-2013.jpg"
+    
+    # === CARTELES DONA UN JUGUETE ===
+    "039_cartel_dona_un_juguete_2017.jpg" = "cartel-dona-juguete-2017.jpg"
+    "071_cartel_dona_un_juguete_2016.JPG" = "cartel-dona-juguete-2016.jpg"
+    "105_CARTEL_CAMPA_C3_91A_DONA_UN_JUGUETE_2014.JPG" = "cartel-dona-juguete-2014.jpg"
+    "143_cartel_recogida_de_juguetes.JPG" = "cartel-dona-juguete-2013.jpg"
+    
+    # === CARTELES PAPELETAS / RIFAS ===
+    "081_papeleta.jpg" = "cartel-papeleta-rifa-2015.jpg"
+    "115_papeleta_rifa_bicicleta2014.JPG" = "cartel-papeleta-rifa-2014.jpg"
+    "158_papeleta_rifa_bicicleta.jpg" = "cartel-papeleta-rifa-2013.jpg"
+    
+    # === CARTELES 50 ANIVERSARIO ===
+    "059_CARTEL_50_ANIVERSARIO_RESUCITADO.jpg" = "cartel-50-aniversario-2016.jpg"
+    "060_DIPTICO_50_ANIVERSARIO_RESUCITADO.JPG" = "cartel-50-aniversario-diptico-2016.jpg"
+    "061_CARTEL_PRESENTACION_ACTOS.jpg" = "cartel-presentacion-actos-2016.jpg"
+    "074_actos_provisionales_50_ANIVERSARIO_RESUCITADO.JPG" = "cartel-50-aniversario-actos-2016.jpg"
+    "075_logo_50_aniversario.JPG" = "logo-50-aniversario-2016.jpg"
+    "077_logo_50_aniversario.JPG" = "logo-50-aniversario-2-2016.jpg"
+    
+    # === CARTELES CONFERENCIAS / TERTULIAS ===
+    "015_CARTEL_CONFERENCIA_2018.jpg" = "cartel-conferencia-2018.jpg"
+    "051_Cartel_conferencia_La_Resurrecci_25C3_25B3n.jpg" = "cartel-conferencia-2016.jpg"
+    "052_cartel_tertulia_cuadrilla_resucitado.jpg" = "cartel-tertulia-cuadrilla-2016.jpg"
+    
+    # === CARTELES SAN ANTÓN ===
+    "067_San_Anton_2016.jpg" = "cartel-san-anton-2016.jpg"
+    "101_CARTEL_SAN_ANTON_2015.JPG" = "cartel-san-anton-2015.jpg"
+    "138_SAN_ANTON_2014_-_ERMITA_VIRGEN_DE_GRACIA.JPG" = "cartel-san-anton-2014.jpg"
+    
+    # === CARTELES COMIDA DE HERMANDAD ===
+    "008_Cartel_reserva_comida_hermandad_2019.jpg" = "cartel-comida-hermandad-2019.jpg"
+    "122_CARTEL_COMIDA_DE_HERMANDAD.JPG" = "cartel-comida-hermandad-2014.jpg"
+    
+    # === CARTELES MISAS / DIFUNTOS ===
+    "152_cartel_misa_difuntos_2013.JPG" = "cartel-misa-difuntos-2013.jpg"
+    "109_Cartel_misa_de_difuntos_2014.JPG" = "cartel-misa-difuntos-2014.jpg"
+    
+    # === CARTELES ORLAS DIFUNTOS ===
+    "002_orla_difuntos_2019.png" = "cartel-orla-difuntos-2019.png"
+    "017_orla_difuntos_2018.png" = "cartel-orla-difuntos-2018.png"
+    "025_orla_difuntos_2017.png" = "cartel-orla-difuntos-2017.png"
+    "041_orla_difuntos_2016.png" = "cartel-orla-difuntos-2016.png"
+    "076_orla_difuntos_2015.png" = "cartel-orla-difuntos-2015.png"
+    
+    # === CARTELES ENSAYOS ===
+    "014_CUADRANTE_ENSAYOS_2019.jpg" = "cartel-ensayos-2019.jpg"
+    "034_ensayo_2.jpg" = "cartel-ensayo-2017.jpg"
+    "103_Ensayos_costaleros_2015.JPG" = "cartel-ensayos-2015.jpg"
+    "135_FECHAS_ENSAYOS_COSTALEROS.JPG" = "cartel-ensayos-2014.jpg"
+    "068_FECHAS_ENSAYOS_COSTALEROS_2016.JPG" = "cartel-ensayos-2016.jpg"
+    
+    # === CARTELES BENDICIONES ===
+    "005_CARTEL_BENDICI_25C3_2593N_RETABLO.jpeg" = "cartel-bendicion-retablo-2019.jpg"
+    "095_Cartel_bendici_C3_B3n_obra.JPG" = "cartel-bendicion-obra-2015.jpg"
+    "130_CARTEL_BENDICI_C3_93N__C3_81NGEL_ANUNCIADORbueno.JPG" = "cartel-bendicion-angel-2014.jpg"
+    "063_Cartel_bendicion_Mar_25C3_25ADa_Magdalena.JPG" = "cartel-bendicion-magdalena-2015.jpg"
+    
+    # === CARTELES ESPECIALES ===
+    "021_CARTEL_CONCURSO_DIBUJO.jpg" = "cartel-concurso-dibujo-2018.jpg"
+    "012_dibujo_redes_sociales_con_cuadros.jpeg" = "cartel-dibujo-redes-2019.jpg"
+    "072_cartel_borriquita.jpg" = "cartel-borriquita-2015.jpg"
+    "024_CARTEL_FISIOTERAPIA_COFRADE2017.png" = "cartel-fisioterapia-2017.png"
+    "092_CARTEL_FISIOTERAPIA_Y_COSTAL2015.JPG" = "cartel-fisioterapia-2015.jpg"
+    "132_CARTEL_FISIOTERAPIA_Y_COSTAL.JPG" = "cartel-fisioterapia-2014.jpg"
+    "140_cartel_torneo_navidad_futbol_7.JPG" = "cartel-torneo-navidad-2014.jpg"
+    "050_CARTEL_SALIDA_EXTRAORDINARIA_25JUN.jpg" = "cartel-salida-extraordinaria-2016.jpg"
+    "054_cartel_exaltacion_cofradia.jpg" = "cartel-exaltacion-cofradia-2016.jpg"
+    "053_CARTEL_REUNION_MANTILLAS.JPG" = "cartel-reunion-mantillas-2016.jpg"
+    "116_10557457_774510072601197_9044905814246617635_n.jpg" = "cartel-maraton-equipo-2014.jpg"
+    
+    # === CARTELES COMUNICADOS / DOCUMENTOS OFICIALES ===
+    "011_COMUNICADO_SUBVENCION_AYUNTAMIENTO_2018.jpg" = "doc-comunicado-subvencion-2018.jpg"
+    "013_COMUNICACI_25C3_2593N_HERMANO_MAYOR_HONOR_2019.jpg" = "doc-comunicado-hermano-mayor-honor-2019.jpg"
+    "036_COMUNICADO_BANDA_2017.jpg" = "doc-comunicado-banda-2017.jpg"
+    "006_COMUNICADO_ACLARATORIO_PARTIDO_POLITICO.jpg" = "doc-comunicado-aclartorio-politico.jpg"
+    "100_COMUNICACI_C3_93N_OBRAS_EN_LA_ERMITA.JPG" = "doc-comunicado-obras-ermita-2014.jpg"
+    
+    # === DOCUMENTOS JUNTA DE GOBIERNO ===
+    "037_COMPOSICION_JUNTA_DE_GOBIERNO_2016.jpg" = "doc-junta-gobierno-2016.jpg"
+    "137_COMPOSICION_JUNTA_DE_GOBIERNO_2014.JPG" = "doc-junta-gobierno-2014.jpg"
+    "033_CARTEL_TOMA_DE_POSESI_25C3_2593N_2017.jpeg" = "doc-toma-posesion-2017.jpg"
+    
+    # === DOCUMENTOS CABILDO ===
+    "001_cabildo_general_ordinario_2020.jpg" = "doc-cabildo-ordinario-2020.jpg"
+    "023_cabildo_general_ordinario_2018.jpg" = "doc-cabildo-ordinario-2018.jpg"
+    "035_CABILDO_GENERAL_ORDINARIO_2017.jpg" = "doc-cabildo-ordinario-2017.jpg"
+    "069_cabildo_general_ordinario_2016.JPG" = "doc-cabildo-ordinario-2016.jpg"
+    "102_cabildo_general_ordinario_2015.JPG" = "doc-cabildo-ordinario-2015.jpg"
+    "134_Cabildo_General_Ordinario_15022014.JPG" = "doc-cabildo-ordinario-2014.jpg"
+    "016_cabildo_general_extraordinario_2018.jpg" = "doc-cabildo-extraordinario-2018.jpg"
+    "045_cabildo_general_extraordinario_2016.jpg" = "doc-cabildo-extraordinario-2016.jpg"
+    
+    # === DOCUMENTOS ACTAS / ELECCIONES ===
+    "040_acta_resultado_elecciones.jpg" = "doc-acta-elecciones-2016.jpg"
+    "042_elecciones_resucitado_2016.jpg" = "doc-elecciones-2016.jpg"
+    "106_carta_de_los_reyes_magos.JPG" = "doc-carta-reyes-magos-2014.jpg"
+    
+    # === FOTOS VARIAS ===
+    "157_preparada_cata.jpg" = "foto-preparativos-cata-2013.jpg"
+    "090_BUENA_IGLESIA.JPG" = "foto-ermita-interior-2015.jpg"
+    "118_la_azulita_alcolea.JPG" = "foto-azulita-alcolea-2014.jpg"
+    "119_mapet_el_carpio.JPG" = "foto-mapet-el-carpio-2014.jpg"
+    "142__C3_ADndice.jpg" = "foto-indice-2013.jpg"
+    "047_dorsal_DIA_DE_LA_BICICLETA.jpg" = "foto-dorsal-bicicleta-2016.jpg"
+    "049_dorsal_DIA_DE_LA_BICICLETA.jpg" = "foto-dorsal-bicicleta-2016-2.jpg"
+    "155_actos_caseta_de_feria_2013.jpg" = "foto-programa-caseta-2013.jpg"
+    "112_PROGRAMA_CASETA_DE_FERIA_2014.JPG" = "foto-programa-caseta-2014.jpg"
+    "108_Cartel_presentaci_C3_B3n_Grupo_Joven.JPG" = "foto-presentacion-grupo-joven-2014.jpg"
+    
+    # === CARTELES FERIA / ACTOS ===
+    "044_cartel_feria_2016.jpg" = "cartel-feria-2016.jpg"
+    
+    # === CARTELES SALIDAS / REDENCIÓN ===
+    "031_redencion_2017.jpg" = "cartel-redencion-2017.jpg"
+    
+    # === CARTELES MISCELÁNEOS ===
+    "073_11032000_10155431689315227_3051369484768933633_n.jpg" = "foto-publicacion-facebook-2015.jpg"
+    "065_10981348_547795558696294_9138442707438672064_n.jpg" = "foto-publicacion-facebook-2015-2.jpg"
+    "093_11021255_421988741296772_1649639799594952022_n.jpg" = "foto-publicacion-facebook-2014.jpg"
+    "094_11025157_421988751296771_6377883746944457240_n.jpg" = "foto-cinta-cofradia-2014.jpg"
+    
+    # === CARTELES PROGRAMAS / EVENTOS ESPECÍFICOS ===
+    "123_10302460_295463290615985_4286303086237437630_n.jpg" = "foto-ermita-cruz-2014.jpg"
+}
+
+$sourceDir = "C:\Users\Juanjo\cofradia\Cofradia\imagenes_provicionales"
+
+foreach ($oldName in $renames.Keys) {
+    $newName = $renames[$oldName]
+    $oldPath = Join-Path $sourceDir $oldName
+    $newPath = Join-Path $sourceDir $newName
+    
+    if (Test-Path $oldPath) {
+        Rename-Item -Path $oldPath -NewName $newName -Force
+        Write-Host "Renombrado: $oldName -> $newName"
+    } else {
+        Write-Host "NO ENCONTRADO: $oldName" -ForegroundColor Red
+    }
+}
+
+Write-Host "`nProceso completado. Verificando..." -ForegroundColor Green
+
+# Listar archivos que no fueron renombrados
+$remaining = Get-ChildItem -Path $sourceDir | Where-Object { $_.Name -match '^\d{3}_' }
+if ($remaining.Count -gt 0) {
+    Write-Host "`nArchivos sin renombrar ($($remaining.Count)):" -ForegroundColor Yellow
+    $remaining | ForEach-Object { Write-Host "  - $($_.Name)" }
+} else {
+    Write-Host "`nTodos los archivos fueron renombrados correctamente." -ForegroundColor Green
+}
+
+# Contar por categoría
+$allFiles = Get-ChildItem -Path $sourceDir
+$carteles = $allFiles | Where-Object { $_.Name -like 'cartel-*' }
+$fotos = $allFiles | Where-Object { $_.Name -like 'foto-*' }
+$docs = $allFiles | Where-Object { $_.Name -like 'doc-*' }
+$otros = $allFiles | Where-Object { $_.Name -notlike 'cartel-*' -and $_.Name -notlike 'foto-*' -and $_.Name -notlike 'doc-*' -and $_.Name -notlike 'escudo-*' -and $_.Name -notlike 'logo-*' }
+
+Write-Host "`nResumen:" -ForegroundColor Green
+Write-Host "  Carteles: $($carteles.Count)" -ForegroundColor Cyan
+Write-Host "  Fotos: $($fotos.Count)" -ForegroundColor Cyan
+Write-Host "  Documentos: $($docs.Count)" -ForegroundColor Cyan
+Write-Host "  Otros: $($otros.Count)" -ForegroundColor Cyan
+Write-Host "  Total: $($allFiles.Count)" -ForegroundColor Green
